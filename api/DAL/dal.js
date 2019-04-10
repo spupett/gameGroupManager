6a3f98622ce975ddbf65f96ad9cdb0a41727bae8
Austin Williams <adwilliams@sedonagroup.com>
2019-04-10 14:10:26 -0500

    created DAL

--- api/DAL/dal.js
+++ api/DAL/dal.js
@@ -2,18 +2,39 @@ const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb+srv://admin:${process.env.mongo_pw}@cluster0-fedx8.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true});
 
-DAL = {
-  find: () => {
-
+module.exports = {
+  find: (model) => {
+    return model.find().exec()
+      .then((results) => { return results; })
+      .catch((error) => { throw error; });
   },
 
-  findOne: () => {
-
+  findOne: (model, parameters) => {
+    return model.findOne(parameters).exec()
+      .then((result) => { return result; })
+      .catch((error) => { throw error; })
   },
 
-  save: () => {
+  save: (model, additions) => {
+    if(Array.isArray(additions)) {
+      if(additions.indexOf('_id') !== -1) {
+        model._id = new mongoose.Types.ObjectId();
+      }
+    }
+    return model.save()
+      .then((results) => { return results; })
+      .catch((error) => { throw error; });
+  },
 
+  update: (model, where, update) => {
+    return model.update(where, {$set: update})
+      .then((result) => { return result; })
+      .catch((error) => { throw error; })
+  },
+
+  delete: (model, userName) => {
+    return model.remove({bggName: userName}).exec()
+      .then((results) => { return results; })
+      .catch((error) => { throw error;} );
   }
 }
-
-module.exports = DAL;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const DAL = require('../DAL/dal');
-
 const User = require('../models/user');
 
-const bggController = require('../controllers/bggController');
-
 router.get('/', (req, res, next) => {
-  User.find().exec()
+  DAL.find(User)
     .then((results) => {
       res.status(200).json(results);
     })
@@ -18,35 +15,55 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:userName', (req, res, next) => {
-  User.findOne({bggName: req.params.userName}).exec()
+  DAL.findOne(User, {bggName: req.params.userName})
     .then((result) => {
       res.status(200).json(result);
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).json({ Error: { message: error}})
+      res.status(500).json({ Error: { message: error }})
     });
-
-  // const user = bggController.getUser(req.params.userName);
-  // user.then((response) => {
-  //   res.status(200).json({message: 'found', user: JSON.parse(response)});
-  // })
 });
 
 router.post('/', (req, res, next) => {
   const user = new User({
-    _id: new mongoose.Types.ObjectId(),
     bggName: req.body.bggName,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email
   });
-  user.save().then((result) => {
-    res.status(201).json(result);
-  }).catch((err) => {
-    console.log(error);
-    res.status(500).json({ Error: { message: error}})
-  });
+
+  // Need to add a _id (mongoDB specific)
+  DAL.save(user, ['_id'])
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ Error: { message: error }})
+    })
+});
+
+router.put('/:userName', (req, res, next) => {
+  DAL.update(User, {bggName: req.params.userName}, req.body)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ Error: { message: error }})
+    })
+});
+
+router.delete('/:userName', (req, res, next) => {
+  DAL.delete(User, req.params.userName)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ Error: { message: error }})
+    });
 });
 
 module.exports = router;
